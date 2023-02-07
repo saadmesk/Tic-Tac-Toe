2345f08ca98b5d12fa0ad673b8eb2d2586390943
saadmesk <91161917+saadmesk@users.noreply.github.com>
2023-02-07 13:46:46 -0500

    Update dice.js

--- yatzy/public/assets/dice.js
+++ yatzy/public/assets/dice.js
@@ -1,11 +1,21 @@
 class Dice {
-    constructor() {
-      this.value = null;
-    }
-  
-    roll() {
-      this.value = Math.floor(Math.random() * 6) + 1;
-    }
+  constructor() {
+    this.value = null;
   }
-  
-  module.exports = Dice;
+
+  roll() {
+    this.value = Math.floor(Math.random() * 6) + 1;
+  }
+
+  static throw(numDices) {
+    const dices = [];
+    for (let i = 0; i < numDices; i++) {
+      const dice = new Dice();
+      dice.roll();
+      dices.push(dice);
+    }
+    return dices;
+  }
+}
+
+module.exports = Dice;
